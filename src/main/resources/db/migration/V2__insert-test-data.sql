c836318de3e71a14e6df3a43ac686ae43069cade
KimSeongJin(Dark) <ekdxhrl@coupang.com>
2016-08-17 22:53:14 +0900

    Flyway 한글 insert test data

--- src/main/resources/db/migration/V2__insert-test-data.sql
+++ src/main/resources/db/migration/V2__insert-test-data.sql
@@ -1,8 +1,8 @@
 INSERT INTO users (birth_date, name, password, region_id, room_id, sex, token, user_id) VALUES ("890523", "KimSungJin", "3bfb730ef7a648b784fb249e11b79cc1", "1", "1", "male", "e68e85be-3506-4f4d-a69c-e19408af2e63", "ekdxhrl");
 
-INSERT INTO room (description, name, notice, owner_id, region_id) VALUES ("description", "room1", "notice!", "1", "1");
-INSERT INTO room (description, name, notice, owner_id, region_id) VALUES ("description", "room2", "notice!", "1", "1");
-INSERT INTO room (description, name, notice, owner_id, region_id) VALUES ("description", "room3", "notice!", "1", "2");
+INSERT INTO room (description, name, notice, owner_id, region_id) VALUES ("description", "룸1", "notice!", "1", "1");
+INSERT INTO room (description, name, notice, owner_id, region_id) VALUES ("description", "룸2", "notice!", "1", "1");
+INSERT INTO room (description, name, notice, owner_id, region_id) VALUES ("description", "룸3", "notice!", "1", "2");
 
 INSERT INTO article (category_id, contents, creation_date, name, room_id, view_count, writer_id) VALUES ("1", "contents", "2016-08-17 22:32:38", "text 1", "1", "1", "1");
 INSERT INTO article (category_id, contents, creation_date, name, room_id, view_count, writer_id) VALUES ("1", "contents", "2016-08-17 22:32:38", "text 2", "1", "12", "1");
