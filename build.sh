c2ef0a3e12ef80706fb1d461ab378d24a3ccb096
KimSeongJin(Dark) <ekdxhrl@coupang.com>
2016-08-21 19:39:43 +0900

    build script update

--- build.sh
+++ build.sh
@@ -1,5 +1,24 @@
 #!/usr/bin/env bash
 
+echo "git pull start!"
+
+git pull
+
+echo "git pull! Success!"
+
+echo "flyway start~!!"
+
+`pwd`/gradlew flywayClean
+`pwd`/gradlew flywayMigrate
+
+echo "flyway Success~!!"
+
 `pwd`/gradlew build
 
 docker build -t neighborhood:latest .
+
+docker stop neighborhood-server
+
+docker rm neighborhood-server
+
+docker run -d --net=host --name neighborhood-server -p 8080:8080 neighborhood
