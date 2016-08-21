0a58426a297a8b5059e3765be62f230b99d60986
KimSeongJin(Dark) <ekdxhrl@coupang.com>
2016-08-21 16:21:12 +0900

    create schema 변경 완료

--- src/main/resources/db/migration/V1__create-schema.sql
+++ src/main/resources/db/migration/V1__create-schema.sql
@@ -29,23 +29,23 @@ CREATE TABLE neighborhood.user_region (
 
 CREATE TABLE neighborhood.category (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `category_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `type` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   PRIMARY KEY (`id`)
 );
 
 CREATE TABLE neighborhood.article (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `category_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `article_main_image` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `category_id` bigint(20) DEFAULT NULL,
   `contents` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `creation_date` datetime DEFAULT NULL,
-  `image_url` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `created_at` datetime DEFAULT NULL,
   `name` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
   `region_id` bigint(20) DEFAULT NULL,
   `view_count` bigint(20) DEFAULT NULL,
-  `user_id` varchar(255) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `user_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  FOREIGN KEY (`region_id`) REFERENCES `region` (`id`)
+  FOREIGN KEY (`region_id`) REFERENCES `regions` (`id`),
+  FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
 );
 
 CREATE TABLE neighborhood.comment (
@@ -57,4 +57,4 @@ CREATE TABLE neighborhood.comment (
   PRIMARY KEY (`id`),
   FOREIGN KEY (`article_id`) REFERENCES `article` (`id`),
   FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
-);
+)
