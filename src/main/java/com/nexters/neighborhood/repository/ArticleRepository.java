0de5da8bc6363b24b22c25ad635147eb38244f4e
KimSeongJin(Dark) <ekdxhrl@coupang.com>
2016-08-27 02:16:00 +0900

    모든 Article 순서를 최신순으로 한다

--- src/main/java/com/nexters/neighborhood/repository/ArticleRepository.java
+++ src/main/java/com/nexters/neighborhood/repository/ArticleRepository.java
@@ -15,9 +15,11 @@ public interface ArticleRepository extends JpaRepository<Article, Long>{
     @Query(value = "SELECT * FROM article a WHERE a.region_id = :regionId order by created_at desc", nativeQuery = true)
     List<Article> findByRegionId(@Param("regionId") Long regionId);
 
-    List<Article> findByRegionIdAndCategoryId(Long regionId, Long id);
+    @Query(value = "SELECT * FROM article a WHERE a.region_id = :regionId AND a.category_id = :categoryId order by created_at desc", nativeQuery = true)
+    List<Article> findByRegionIdAndCategoryId(@Param("regionId") Long regionId,@Param("categoryId") Long categoryId);
 
-    List<Article> findByUserId(Long userId);
+    @Query(value = "SELECT * FROM article a WHERE a.user_id = :userId order by created_at desc", nativeQuery = true)
+    List<Article> findByUserId(@Param("userId") Long userId);
 
     @Query(value = "SELECT * FROM article a WHERE a.title LIKE CONCAT('%',:title,'%')", nativeQuery = true)
     List<Article> findLikeTitle(@Param("title") String title);
